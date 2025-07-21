7b3123d9f25ddf381a7e3406d709ec5f181a460b
Newton Wamiti <144038101+Wamitinewton@users.noreply.github.com>
2025-07-21 16:13:56 +0300

    init

---
+++ spring-fcm/src/test/java/com/newton/springfcm/SpringFcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newton.springfcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
